d2f3890b0c564ced68fa47fc5efab55b5272ea37
Gleb <m_marple1@mail.ru>
2024-08-03 14:48:57 +1000

    New app name and test view's func

--- PythonBytes/blog/urls.py
+++ PythonBytes/blog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
+app_name = 'blog'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
+    path('', views.home, name='home'),
 ]
