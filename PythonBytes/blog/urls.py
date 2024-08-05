4a94694768914d0cefca59d83009cabd32f38327
Gleb <m_marple1@mail.ru>
2024-08-05 13:04:52 +1000

    add a detailed view for posts

--- PythonBytes/blog/urls.py
+++ PythonBytes/blog/urls.py
@@ -7,4 +7,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    # path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    # path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    # path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- PythonBytes/blog/views.py
+++ PythonBytes/blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.backends.django import reraise
+from django.views.generic import DetailView
+
 from .models import Post
 
 
@@ -15,3 +17,23 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+
+class PostCreateView():
+    pass
+
+
+
+class PostUpdateView():
+    pass
+
+
+
+class PostDeleteView():
+    pass
